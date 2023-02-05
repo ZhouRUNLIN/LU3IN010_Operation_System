4a36f78c2cfdc7678660f32932b3c8120053e541
RUNLIN ZHOU <72131094+ZhouRUNLIN@users.noreply.github.com>
2023-02-05 11:59:09 +0100

    Add files via upload

--- TME2/lanceCommande.c
+++ TME2/lanceCommande.c
@@ -7,7 +7,11 @@
 #include "lanceCommande.h"
 
 void lanceCommande(char* commande){
+    // les codes pour exercice 2 
+    time_t start, end;
+    time(&start);
     int status = system(commande);
+    time(&end);
     
     if (-1 == status){       
         printf("system error!");
@@ -22,16 +26,18 @@ void lanceCommande(char* commande){
         }
     }
     // les codes precedent peuvent renvoyer un message d’erreur si la commande n’a pu être exécutée correctement.
-
-    time_t start, end;
-    time(&start);
-    system(commande);
-    time(&end);
     printf("Command : %s\nTime for execution : %ld\n", commande, end-start);
 }
 
 void lanceCommandeNew(char *commande){
+    // les codes pour exercice 2 
+	struct tms *startTms = malloc(sizeof(struct tms));
+	struct tms *endTms = malloc(sizeof(struct tms));
+	clock_t tStart,tEnd;	
+	
+	tStart = times(startTms);
     int status = system(commande);
+    tEnd = times(endTms);
     
     if (-1 == status){       
         printf("system error!");
@@ -46,28 +52,18 @@ void lanceCommandeNew(char *commande){
         }
     }
     // les codes precedent peuvent renvoyer un message d’erreur si la commande n’a pu être exécutée correctement.
-    
-    clock_t start, end;
-    typedef struct tms TMS;
-    TMS start2, end2;
-    struct timeval debut, fin;
-    
-    
-    gettimeofday(&debut, NULL);
-    times(&start2);
-    system(commande);
-    gettimeofday(&fin, NULL);
-    times(&end2);
+    fprintf(stdout,"\nStatistiques de %s:\n", commande);
+	fprintf(stdout,"Temps total : %6.6f\n",
+		(double)(tEnd-tStart)/ CLOCKS_PER_SEC);
+	fprintf(stdout,"Temps utilisateur : %6.6f\n", 
+		(double)(endTms->tms_utime - startTms->tms_utime)/ CLOCKS_PER_SEC);
+	fprintf(stdout,"Temps systeme : %6.6f\n",
+		(double)(endTms->tms_stime - startTms->tms_stime)/ CLOCKS_PER_SEC);
+	fprintf(stdout,"Temps utilisateur fils : %6.6f\n",
+		(double)(endTms->tms_cutime - startTms->tms_cutime)/ CLOCKS_PER_SEC);
+	fprintf(stdout,"Temps systeme fils : %6.6f\n",
+		(double)(endTms->tms_cstime - startTms->tms_cstime)/ CLOCKS_PER_SEC);
 
-    long int tick_per_sec = sysconf(_SC_CLK_TCK);
-    long int delt_u = fin.tv_usec - debut.tv_usec;
-    long int delt_s = fin.tv_sec - debut.tv_sec;
-    double temps_total = delt_s + delt_u*1./CLOCKS_PER_SEC;
-    printf("Commande : %s\n", commande);
-    printf("Temps total: %.6lf\n", temps_total);
-    printf("Temps utilisateur: %.6lf\n", (double)(end2.tms_utime - start2.tms_utime)/tick_per_sec);
-    printf("Temps système: %.6lf\n", (double)(end2.tms_stime - start2.tms_stime)/tick_per_sec);
-    printf("Temps utilisateur fils: %.6lf\n", (double)(end2.tms_cutime - start2.tms_cutime)/tick_per_sec);
-    printf("Temps système fils: %.6lf\n", (double)(end2.tms_cstime - start2.tms_cstime)/tick_per_sec);
-    
+    free(startTms);
+    free(endTms);
 }
